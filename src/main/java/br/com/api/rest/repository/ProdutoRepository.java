35d5fb6dcc00c64b8cfcd37d722fbc955205328c
Tiago Santos <tiago.santos@farminvestimentos.com.br>
2022-10-06 15:00:00 -0300

    Criando interface para persistencia no BD

---
+++ src/main/java/br/com/api/rest/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package br.com.api.rest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.api.rest.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
