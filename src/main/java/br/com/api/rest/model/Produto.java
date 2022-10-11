bebf8a2892fb490a077b92223266254cb94732cd
Tiago Santos <pereirat476@gmail.com>
2022-10-10 23:25:51 -0300

    trocando os getter e setter por @Getter e @Setter com Lombok

--- src/main/java/br/com/api/rest/model/Produto.java
+++ src/main/java/br/com/api/rest/model/Produto.java
@@ -1,5 +1,8 @@
 package br.com.api.rest.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 
@@ -9,8 +12,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Produto implements Serializable {	
-	
+@Getter
+@Setter
+public class Produto implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -18,32 +23,5 @@ public class Produto implements Serializable {
 	private String nome;
 	private BigDecimal quantidade;
 	private BigDecimal valor;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public BigDecimal getQuantidade() {
-		return quantidade;
-	}
-	public void setQuantidade(BigDecimal quantidade) {
-		this.quantidade = quantidade;
-	}
-	public BigDecimal getValor() {
-		return valor;
-	}
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-	
-	
-	
-	
+
 }
